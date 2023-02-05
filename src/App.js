6940932c4ed77ec8079f7957de94387ac8f5b43f
liibanomar28@gmail.com <liibanomar288>
2023-02-05 23:12:36 +0300

    n

--- src/App.js
+++ src/App.js
@@ -1,8 +1,11 @@
 import Jobcard from "./components/Jobcard";
+import AdsComponent from "./components/Adsense";
 function App() {
+
   return (
     <div className="flex bg-slate-200 flex-1 items-center justify-center flex-col h-auto space-y-5">
-      
+      <h1>Place To show Google AdSense</h1>
+           <AdsComponent dataAdSlot='f08c47fec0942fa0' />
               <div className="w-full flex-row flex items-center justify-center bg-black space-x-20  pt-10  pl-5 h-[100px] ">
                 <h1 className="text-yellow-500 font-bold text-2xl">Nalka Shid</h1>
                 <input type="text" className="ml-5 text-yellow-500 bg-slate-500 rounded-xl h-[30px] w-[40%] pl-5"placeholder="Search"/>
@@ -10,15 +13,15 @@ function App() {
               </div>
               <div className="w-[80%] h-full">
         <div className=" bg-white opacity-80 h-[400px] flex-col " >
-      <h2 className="text-center font-extrabold text-yellow-500 text-[50px] font-sans">Trending Videos</h2>
+      <h2 className="text-center font-extrabold text-yellow-500 text-[40px] font-sans">Trending Videos</h2>
 
     
 
         </div>          
 
         <div className="bg-white mt-10 py-20 space-y-10 h-full w-full items-center justify-center flex flex-col">
-          <h1 className="text-3xl font-bold "> Maanta Videos</h1>
-          <div className="grid grid-cols-4 gap-4">
+          <h1 className="text-3xl font-bold text-red-600"> Maanta Videos</h1>
+          <div className="grid grid-cols-4 gap-4 space-x-2">
           <Jobcard/>
           <Jobcard/>
           <Jobcard/>
@@ -28,7 +31,7 @@ function App() {
           <Jobcard/>
           <Jobcard/>  
           </div>
-          <h1 className="text-3xl font-bold mt-5 "> Yesterday Videos</h1>
+          <h1 className="text-3xl font-bold mt-5 text-red-600 "> Yesterday Videos</h1>
           <div className="grid grid-cols-4 gap-4">
           <Jobcard/>
           <Jobcard/>
--- src/components/Jobcard.js
+++ src/components/Jobcard.js
@@ -2,14 +2,12 @@ import React from 'react'
 
 function Jobcard() {
   return (
-    <div className="bg-white h-[500px] w-[500px] shadow-lg flex flex-col" >
+    <div className="bg-white h-[500px] w-[200px] shadow-lg flex flex-col" >
       <div className="h-full w-full  bg-blue-100"></div>
       <div className="flex-col pl-5  pt-2 h-full w-full bg-white items-center justify-center">
-        <h2 className="text-2xl text-center font-bold pt-8">He's Seeing Stars: Dude Gets Knocked Out Within 2 Seconds During A Boxing Match!</h2>
-        <h2 className="text-gray-400 text-center text-lg pt-10 font-sans">Seen 2000 Times</h2>
-
+      <h2 className="text-2xl text-center font-bold pt-8">He's Seeing Stars:Lewandoski scores 5 in 9 MINUTES!</h2>
+      <h2 className="text-gray-400 text-center text-lg pt-10 font-sans"></h2>
     </div>
-
   </div>
   )
 }
