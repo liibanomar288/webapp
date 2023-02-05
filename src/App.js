a6a17b7e1fecd01ffddd13b5fde0e8f338e15ea6
liibanomar28@gmail.com <liibanomar288>
2023-02-05 19:21:42 +0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Jobcard from "./components/Jobcard";
+import main from "./components/main.jpg"
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="flex bg-slate-200 flex-1 items-center justify-center flex-col h-auto space-y-5">
+      
+              <div className="w-full flex-row flex items-center justify-center bg-black space-x-20  pt-10  pl-5 h-[100px] ">
+                <h1 className="text-yellow-500 font-bold text-2xl">Nalka Shid</h1>
+                <input type="text" className="ml-5 text-yellow-500 bg-slate-500 rounded-xl h-[30px] w-[40%] pl-5"placeholder="Search"/>
+
+              </div>
+              <div className="w-[80%] h-full">
+        <div className=" bg-white opacity-80 h-[400px] flex-col " >
+      <h2 className="text-center font-extrabold text-yellow-500 text-[50px] font-sans">Trending Videos</h2>
+
+    
+
+        </div>          
+
+        <div className="bg-white mt-10 py-20 space-y-10 h-full w-full items-center justify-center flex flex-col">
+          <h1 className="text-3xl font-bold "> Maanta Videos</h1>
+          <div className="grid grid-cols-4 gap-4">
+          <Jobcard/>
+          <Jobcard/>
+          <Jobcard/>
+          <Jobcard/>
+          <Jobcard/>
+          <Jobcard/>
+          <Jobcard/>
+          <Jobcard/>  
+          </div>
+          <h1 className="text-3xl font-bold mt-5 "> Yesterday Videos</h1>
+          <div className="grid grid-cols-4 gap-4">
+          <Jobcard/>
+          <Jobcard/>
+          <Jobcard/>
+          <Jobcard/>
+          <Jobcard/>
+          <Jobcard/>
+          <Jobcard/>
+          <Jobcard/>  
+          </div>
+
+
+        </div>
+        <div className="w-full mt-20  h-[500px] bg-black"></div>
+    </div>
     </div>
   );
 }
---
+++ src/components/Jobcard.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Jobcard() {
+  return (
+    <div className="bg-white h-[500px] w-[500px] shadow-lg flex flex-col" >
+      <div className="h-full w-full  bg-blue-100"></div>
+      <div className="flex-col pl-5  pt-2 h-full w-full bg-white items-center justify-center">
+        <h2 className="text-2xl text-center font-bold pt-8">He's Seeing Stars: Dude Gets Knocked Out Within 2 Seconds During A Boxing Match!</h2>
+        <h2 className="text-gray-400 text-center text-lg pt-10 font-sans">Seen 2000 Times</h2>
+
+    </div>
+
+  </div>
+  )
+}
+
+export default Jobcard
