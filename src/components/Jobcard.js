94d5a107f4b7c78c803c7a42e50d672e676228f6
Liiban <45320408+liibanomar288@users.noreply.github.com>
2023-02-05 23:31:10 +0300

    Update Jobcard.js

--- src/components/Jobcard.js
+++ src/components/Jobcard.js
@@ -5,8 +5,8 @@ function Jobcard() {
     <div className="bg-white h-[500px] w-[200px] shadow-lg flex flex-col" >
       <div className="h-full w-full  bg-blue-100"></div>
       <div className="flex-col pl-5  pt-2 h-full w-full bg-white items-center justify-center">
-      <h2 className="text-2xl text-center font-bold pt-8">He's Seeing Stars:Lewandoski scores 5 in 9 MINUTES!</h2>
-      <h2 className="text-gray-400 text-center text-lg pt-10 font-sans"></h2>
+      <p className="text-2xl text-center font-bold pt-8">He's Seeing Stars:Lewandoski scores 5 in 9 MINUTES!</p>
+      <p className="text-gray-400 text-center text-lg pt-10 font-sans"></p>
     </div>
   </div>
   )
